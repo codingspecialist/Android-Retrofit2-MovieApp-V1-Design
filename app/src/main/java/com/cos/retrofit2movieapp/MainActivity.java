9bbdd63f71d5b8b32d4a52077adcafa7962eda91
ssar <ssar@nate.com>
2020-01-03 17:23:56 +0900

    안드로이드 영화 앱 디자인 완성

---
+++ app/src/main/java/com/cos/retrofit2movieapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.cos.retrofit2movieapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
